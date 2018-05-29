fb00110429fc7e314a3151c6af9f4c10f33bb856
uranoni <uranoni777@gmail.com>
2018-05-29 08:51:31 +0800

    add server.js by Express and define scheme for graphql to use

---
+++ schema.js
@@ -0,0 +1,101 @@
+import {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema
+} from 'graphql';
+
+import Db from './db';
+
+const Person = new GraphQLObjectType({
+  name:'Person',
+  description:'this represents a Person',
+  fields:()=>{
+    return {
+      id:{
+        type:GraphQLInt,
+        resolve(person){
+          return person.id;
+        }
+      },
+      firstName:{
+        type:GraphQLString,
+        resolve(person){
+          return person.firstName
+        }
+      },
+      lastName:{
+        type:GraphQLString,
+        resolve(person){
+          return person.lastName
+        }
+      },
+      email:{
+        type:GraphQLString,
+        resolve(person){
+          return person.email
+        }
+      }
+    }
+  }
+})
+
+
+const Post = new GraphQLObjectType({
+  name:'Post',
+  description:'this is a Post',
+  fields:()=>{
+    return {
+      id:{
+        type:GraphQLInt,
+        resolve(person){
+          return person.id;
+        }
+      },
+      title:{
+          type:GraphQLString,
+          resolve(post){
+            return post.title;
+          }
+        
+      },
+      content:{
+          type:GraphQLString,
+          resolve(post){
+            return post.email
+          }
+        
+      }
+    }
+  }
+});
+
+const Query = new GraphQLObjectType({
+  name:'Query',
+  description:'this is a root query',
+  fields:()=>{
+    return{
+      people:{ 
+        type:new GraphQLList(Person),
+        args:{
+          id:{
+            type:GraphQLInt
+          },
+          email:{
+            type:GraphQLString
+          }
+        },
+        resolve(root,args){
+          return Db.models.person.findAll({where:args});
+        }
+      }
+    }
+  }
+})
+
+const Shema = new GraphQLSchema({
+  query:Query
+});
+
+export default Shema;
